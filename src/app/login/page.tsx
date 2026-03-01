de693ce531e3b4fa39275ebc18d05728c85d714e
mc-system-admin <system.admin@magsaysaycollege.edu.ph>
2026-03-01 20:52:10 +0800

    v0.0.021 fix v2

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardFooter, CardHeader } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { supabaseBrowser } from "@/supabase/routeClient";
+import { createSupabaseServerClient } from "@/supabase/server";
 import { useRouter } from "next/navigation";
 import { SubmitEvent, useState } from "react";
 import { toast } from "sonner";
@@ -38,7 +38,9 @@ export default function LoginPage() {
             return;
         }
 
-        const { error } = await supabaseBrowser.auth.signInWithPassword({
+        const supabase = await createSupabaseServerClient();
+
+        const { error } = await supabase.auth.signInWithPassword({
             email,
             password,
         });
--- src/proxy.ts
+++ src/proxy.ts
@@ -1,9 +1,11 @@
-import { createServerClient } from "@supabase/ssr";
+import { createServerClient, type CookieOptions } from "@supabase/ssr";
+import { cookies } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
 
 type Role = "admin" | "user";
 
 export async function proxy(request: NextRequest) {
+    const cookieStore = await cookies();
     const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
     const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
@@ -12,14 +14,14 @@ export async function proxy(request: NextRequest) {
 
     const supabase = createServerClient(supabaseUrl, supabaseKey, {
         cookies: {
-            getAll() {
-                return request.cookies.getAll();
+            get(name: string) {
+                return cookieStore.get(name)?.value;
             },
-            setAll(cookiesToSet) {
-                // Apply cookie changes to the outgoing response
-                cookiesToSet.forEach(({ name, value, options }) => {
-                    response.cookies.set(name, value, options);
-                });
+            set(name: string, value: string, options: CookieOptions) {
+                cookieStore.set({ name, value, ...options });
+            },
+            remove(name: string, options: CookieOptions) {
+                cookieStore.set({ name, value: "", ...options });
             },
         },
     });
